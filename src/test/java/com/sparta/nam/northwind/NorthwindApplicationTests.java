45dfda2e5ee4659f05a43c545921c1585f4ef328
Haashim Alvi <haashimalvi@pm.me>
2025-09-05 13:57:58 +0100

    project init

---
+++ src/test/java/com/sparta/nam/northwind/NorthwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nam.northwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
